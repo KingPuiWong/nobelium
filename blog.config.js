19c615c7dc472e0df27c7891c9d77f22acecdde5
Jarvis <47409734+KingPuiWong@users.noreply.github.com>
2023-09-11 19:31:35 +0800

    feat: blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,9 +2,9 @@ const BLOG = {
   title: 'Beyond Hello World',
   author: 'Jarvis',
   email: 'kingpuii.wong@gmail.com',
-  link: 'https://github.com/KingPuiWong',
+  link: 'https://nonstop-basement-b11.notion.site/ad708f5dba3748a1acf2146d55c2ab43?v=9725a268375047a19fe09ea2e7c67484&pvs=4',
   description: 'This is my blog website.',
-  lang: ['en-US','zh-CN'], // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
